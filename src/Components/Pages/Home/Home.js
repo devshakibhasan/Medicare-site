3f08e33090fdd449ad6511a683cbe3750d845afe
shakib hasan <shakibhasan@gmail.com>
2022-08-24 10:08:49 +0600

    create top home section

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -11,7 +11,23 @@ class Home extends Component {
     render() {
         return (
             <div>
-               
+                <div className='home-top-section'>
+                    <Container>
+                        <Row >
+                            <Col xs={12} sm={12} md={6} lg={6} className='text-center d-flex justify-content-start align-items-center'>
+
+                            </Col>
+                            <Col xs={12} sm={12} md={6} lg={6} className='home-top-right-section'>
+                              
+                                    <h1>
+                                        The Most Valuable Thing is Your Health</h1>
+                                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove.</p>
+                         
+
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
 
 
             </div>
